174e198460fd2698ed524d070b6adba902a3f58d
Max <max@canaery.com>
2022-11-23 15:51:50 -0500

    simplify Indium code

---
+++ zmq_debug/common.py
@@ -0,0 +1,34 @@
+import json
+import numpy as np
+
+
+class NumpyFloatValuesEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.float32):
+            return float(obj)
+        return json.JSONEncoder.default(self, obj)
+
+
+def decode_one_channel(msg, msg_num=None, ch_num=None, channel_nums=None):
+    """
+    Check and decode Json message from open ephys
+    :param msg:
+    :param msg_num:
+    :param ch_num:
+    :return:
+    """
+    if len(msg) < 2:
+        raise ValueError("no frames for message: ", msg[0])
+    header = json.loads(msg[1].decode('utf-8'))
+    if header['type'] != 'data':
+        raise NotImplementedError('Only data allowed')
+    if msg_num is not None and header['message_num'] != msg_num + 1:
+        print("missing a message at number", msg_num)
+    c = header['content']
+    # Check that not missing channels unless this is a first or last channel
+    if ch_num != 0 and ch_num != 135 and ch_num != 543 and (c['channel_num'] != ch_num + 1):
+        print("missing a channel at number", ch_num)
+    channel_nums.append(ch_num)
+    n_arr = np.frombuffer(msg[2], dtype=np.float32)
+    n_arr = np.reshape(n_arr, c['num_samples'])
+    return header['message_num'], c['channel_num'], c['num_samples'], int(c['sample_rate']), n_arr, channel_nums
--- zmq_debug/reporter_new.py
+++ zmq_debug/reporter_new.py
@@ -1,3 +1,4 @@
+from common import decode_one_channel, NumpyFloatValuesEncoder
 from pathlib import Path
 
 import matplotlib.pyplot as plt
@@ -25,6 +26,7 @@ channel_num = 0
 n_small_buffers = 0
 # plt.ion()
 from time import time
+
 time_started = time()
 save_dict = {}
 for i in range(136):
@@ -34,37 +36,6 @@ save_json = True
 timestamps = []
 warmup_till = 51300
 
-
-class NumpyFloatValuesEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.float32):
-            return float(obj)
-        return json.JSONEncoder.default(self, obj)
-
-
-def decode_one_channel(msg, msg_num=None, ch_num=None):
-    """
-    Check and decode Json message from open ephys
-    :param msg:
-    :param msg_num:
-    :param ch_num:
-    :return:
-    """
-    if len(msg) < 2:
-        raise ValueError("no frames for message: ", msg[0])
-    header = json.loads(msg[1].decode('utf-8'))
-    if header['type'] != 'data':
-        raise NotImplementedError('Only data allowed')
-    if msg_num is not None and header['message_num'] != msg_num + 1:
-        print("missing a message at number", msg_num)
-    c = header['content']
-    # Check that not missing channels unless this is a first or last channel
-    if ch_num != 0 and ch_num != 135 and ch_num != 543 and (c['channel_num'] != ch_num + 1):
-        print("missing a channel at number", ch_num)
-    channel_nums.append(ch_num)
-    n_arr = np.frombuffer(msg[2], dtype=np.float32)
-    n_arr = np.reshape(n_arr, c['num_samples'])
-    return header['message_num'], c['channel_num'], c['num_samples'], int(c['sample_rate']), n_arr
 
 
 message_num_received_in_10_sec = 0
@@ -82,7 +53,8 @@ while True:
     if message_num_received < 3e4:
         # Crucial to avoid missing packets! Must be no fewer than 25000 packets or ~8 sec warmup
         continue
-    message_num, channel_num, num_samples, sample_rate, n_arr = decode_one_channel(message, message_num, channel_num)
+    message_num, channel_num, num_samples, sample_rate, n_arr, channel_nums = decode_one_channel(
+        message, message_num, channel_num, channel_nums)
 
     if message_num_received < warmup_till:
         # Collect some info about samples and channels for a second first
@@ -95,14 +67,13 @@ while True:
 
     # this part will help us catch the messages from the beginning of the samples i.e. from channel 0
     if message_num_received == warmup_till:
-       if final_channel_51K < 135:
+        if final_channel_51K < 135:
             chan_left = 135 - final_channel_51K
             warmup_till += chan_left
 
     now = time() - time_started
     if now > 1 and now % 1 < 0.1:
-        # print(f'Received {message_num_received_in_10_sec * num_samples / n_chans} samples per channel in {now} seconds')
-        print(f'Received {message_num_received_in_10_sec * num_samples / n_chans/now} samples per channel per second')
+        print(f'Received {message_num_received_in_10_sec * num_samples / n_chans / now} samples per channel per second')
         time_started = time()
         message_num_received_in_10_sec = 0
 
@@ -122,18 +93,19 @@ while True:
         timestamps.append(time())
         small_buffer = []
     small_buffer.extend(n_arr)
-    
-    #append each channel to a dict and save after 12558 samples or 73710 message
+
+    # append each channel to a dict and save after 12558 samples or 73710 message
     if save_json:
         p = save_dict[int(channel_num)]
         p.extend(n_arr)
         save_dict[int(channel_num)] = p
         # print(f"length of channel {channel_num}: ", len(p))
         if message_num_received == 73710:
-            plt.hist(np.diff(timestamps),100)
+            plt.hist(np.diff(timestamps), 100)
             plt.show()
 
             import time
+
             time.sleep(5)
             # saving the channels in a json file
             json_object = json.dumps(save_dict, cls=NumpyFloatValuesEncoder)
@@ -164,4 +136,3 @@ while True:
             # Clear big buffer
             big_buffer = []
             n_small_buffers = 0
-
