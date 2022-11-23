bfffeb0f2eb6754a16443de488fb21e9a3f98d26
Max <max@canaery.com>
2022-11-23 15:31:25 -0500

    synthetic dataset where signal always rises for 30 seconds

--- zmq_debug/synthetic_datasets/combine_channels.py
+++ zmq_debug/synthetic_datasets/combine_channels.py
@@ -2,35 +2,37 @@ import neo
 from pathlib import Path
 import numpy as np
 
-from neo.core import Segment, SpikeTrain, AnalogSignal
-from quantities import Hz, s
-#neo.OpenEphysIO(Path('~/data/2022-10-20_11-45-33/Record Node 110/experiment1/recording1/continuous/File_Reader-100.Rhythm_FPGA-100.0/continuous.dat').expanduser().__str__()).read()
-fdata=neo.RawBinarySignalIO(Path('~/data/2022-10-20_11-45-33/Record Node 110/experiment1/recording1/continuous/File_Reader-100.Rhythm_FPGA-100.0/continuous.dat').expanduser().__str__(),
-                            sampling_rate=1000.,
-                            nb_channel=136
-                            )
+from neo.core import Segment, AnalogSignal
+from quantities import Hz
 
-seg=fdata.read_segment(0,0)
-large_data= np.hstack((np.array(seg.analogsignals[0]),np.array(seg.analogsignals[0]),np.array(seg.analogsignals[0]),np.array(seg.analogsignals[0])))
+original_data_fpath = 'data/Record Node 110/experiment1/recording1/continuous/File_Reader-100.Rhythm_FPGA-100.0/continuous.dat'
+fdata = neo.RawBinarySignalIO(Path(original_data_fpath).expanduser().__str__(),
+                              sampling_rate=1000.,
+                              nb_channel=136
+                              )
+
+seg = fdata.read_segment(0, 0)
+large_data = np.hstack((np.array(seg.analogsignals[0]), np.array(seg.analogsignals[0]), np.array(seg.analogsignals[0]),
+                        np.array(seg.analogsignals[0])))
 channel_count = large_data.shape[1]
 
-
-sig = neo.AnalogSignal(list(large_data), units='uV', sampling_rate=1 * Hz)
+sig = AnalogSignal(list(large_data), units='uV', sampling_rate=1 * Hz)
 sig.ndim
 segl = Segment(index=5)
 segl.analogsignals.append(sig)
 segl.analogsignals[0].ndim
-Path('~/oebin/continuous/').expanduser().mkdir(exist_ok=True)
-Path('~/oebin/continuous/stream1/').expanduser().mkdir(exist_ok=True)
+Path('data/open_ephys_data_544ch_12sec/').expanduser().mkdir(exist_ok=True)
+Path('data/open_ephys_data_544ch_12sec/continuous/').expanduser().mkdir(exist_ok=True)
+Path('data/open_ephys_data_544ch_12sec/continuous/stream1/').expanduser().mkdir(exist_ok=True)
 
-flarge_data=neo.RawBinarySignalIO(Path('~/oebin/continuous/stream1/continuous.dat').expanduser())
+flarge_data = neo.RawBinarySignalIO(Path(
+    'data/open_ephys_data_544ch_12sec/continuous/stream1/continuous.dat').expanduser())
 flarge_data.write_segment(segl)
 flarge_data
 
-
 from open_ephys.analysis.formats import BinaryRecording
-BinaryRecording.create_oebin_file(Path('~/oebin/').expanduser(),
+
+BinaryRecording.create_oebin_file(Path('data/open_ephys_data_544ch_12sec').expanduser(),
                                   stream_name='stream1',
                                   channel_count=channel_count,
                                   sample_rate=1000)
-
--- zmq_debug/synthetic_datasets/create_synthetic_data.py
+++ zmq_debug/synthetic_datasets/create_synthetic_data.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from neo.core import Segment, SpikeTrain, AnalogSignal
 from quantities import Hz, s
-large_data = np.linspace(0, 20000, 30*1000)  # 30 sec
+large_data = np.linspace(0, 20000, 30*1000).astype('float32')  # 30 sec
 large_data = np.stack(list([large_data])*544).T
 channel_count = large_data.shape[1]
 
@@ -13,16 +13,17 @@ sig.ndim
 segl = Segment(index=5)
 segl.analogsignals.append(sig)
 segl.analogsignals[0].ndim
-Path('~/oebin/continuous/').expanduser().mkdir(exist_ok=True)
-Path('~/oebin/continuous/stream1/').expanduser().mkdir(exist_ok=True)
+Path('data/open_ephys_data_544ch_30sec_synthetic/').expanduser().mkdir(exist_ok=True)
+Path('data/open_ephys_data_544ch_30sec_synthetic/continuous/').expanduser().mkdir(exist_ok=True)
+Path('data/open_ephys_data_544ch_30sec_synthetic/continuous/stream1/').expanduser().mkdir(exist_ok=True)
 
-flarge_data=neo.RawBinarySignalIO(Path('~/oebin/continuous/stream1/continuous.dat').expanduser())
+flarge_data=neo.RawBinarySignalIO(Path('data/open_ephys_data_544ch_30sec_synthetic/continuous/stream1/continuous.dat').expanduser())
 flarge_data.write_segment(segl)
 flarge_data
 
 
 from open_ephys.analysis.formats import BinaryRecording
-BinaryRecording.create_oebin_file(Path('~/oebin/').expanduser(),
+BinaryRecording.create_oebin_file(Path('data/open_ephys_data_544ch_30sec_synthetic/').expanduser(),
                                   stream_name='stream1',
                                   channel_count=channel_count,
                                   sample_rate=1000)
